6ec35a746dfc2162ace781faa7629f56ded5ac3e
sl693 <sl693@exeter.ac.uk>
2024-07-19 17:40:28 +0100

    add generalPlotAlleleSpecifity working plot function

---
+++ 4_Phasing/generalPlotAlleleSpecifity.R
@@ -0,0 +1,41 @@
+plot_allele_perTranscript <- function(inputgtf, transcript, refValue, altValue){
+  dat <- inputgtf[inputgtf$transcript_id == transcript,]
+  
+  dat <- dat[dat$type == "exon",] %>% mutate(value = refValue)
+  
+  inter_exons <- dat %>%
+    mutate(
+      next_start = lead(start) - 1
+    ) %>%
+    filter(!is.na(next_start)) %>%
+    transmute(
+      seqnames = seqnames,
+      start = end + 1,
+      end = next_start,
+      width = end - start + 1,
+      strand = strand,
+      source = source,
+      type = "inter_exon",
+      score = score,
+      phase = phase,
+      gene_id = gene_id,
+      transcript_id = transcript_id
+    )
+  
+  # Combine exon and inter-exon data frames
+  ref_data <- bind_rows(dat, inter_exons) %>% arrange(start) %>% mutate(transcript = transcript)
+  
+  ref_lines <- ref_data %>%
+    select(seqnames, start, end) %>%
+    tidyr::pivot_longer(cols = c(start, end), names_to = "position_type", values_to = "position") %>%
+    mutate(value = ifelse(position_type == "start", refValue, NA),
+           transcript = transcript)
+  
+  ref_data <- ref_data %>% mutate(value = ifelse(is.na(value),NA,refValue), transcript = transcript)
+  alt_data <- ref_data %>% mutate(value = ifelse(is.na(value),NA,altValue), transcript = transcript)
+  alt_lines <- ref_lines %>% mutate(value = ifelse(is.na(value),NA,altValue))
+  
+  output <- list(ref_data, ref_lines, alt_data, alt_lines)
+  names(output) <- c("ref","ref_lines","alt", "alt_lines")
+  return(output)
+}
